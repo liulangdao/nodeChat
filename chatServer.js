6b3fd9bb59e02d831d85572a2f1424f47b048963
liulangdao <langdao5@aol.com>
2017-07-21 20:02:56 +0800

    客户之间的通讯

--- chatServer.js
+++ chatServer.js
@@ -1,9 +1,16 @@
 var net = require('net');
 var chatServer = net.createServer();
+clientList = [];
 chatServer.on('connection',function(client) {
   client.write('Hi!\n');
+
+  clientList.push(client);
+
   client.on('data',function(data) {
-    console.log(data);
+    for(let i=0; i<clientList.length; i+=1){
+      //把数据发送给所以客户端
+      clientList[i].write(data);
+    }
   })
 })
 
